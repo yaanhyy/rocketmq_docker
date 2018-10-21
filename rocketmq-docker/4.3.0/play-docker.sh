21f51fb2ab9899ecf376f83c023b632098acabd6
yaanhyy <yaanhyy@gmail.com>
2018-10-21 22:25:56 +0800

    multi netcard solve

--- rocketmq-docker/4.3.0/play-docker.sh
+++ rocketmq-docker/4.3.0/play-docker.sh
@@ -9,5 +9,5 @@ docker build -t apache/rocketmq-broker:4.3.0 ./rocketmq-broker
 
 # Run namesrv and broker
 
-docker run -d -p 9876:9876 --name rmqnamesrv  apache/rocketmq-namesrv:4.3.0
-docker run -d -p 10911:10911 -p 10909:10909 --name rmqbroker --link rmqnamesrv:namesrv -e "NAMESRV_ADDR=namesrv:9876" apache/rocketmq-broker:4.3.0
+#docker service	create  -p 9876:9876 --name rmqnamesrv --network exchange --replicas 1  -e "NAMESRV_ADDR=rmqnamesrv:9876"   apache/rocketmq-namesrv:4.3.0
+docker service create -p 10911:10911 -p 10909:10909 --name rmqbroker --network exchange --replicas 1 -e "NAMESRV_ADDR=rmqnamesrv:9876" apache/rocketmq-broker:4.3.0
